86d68b80376d420515b164dc893c211c380604e0
Joseph Judge <judgejc@clarkson.edu>
2023-01-09 11:08:09 -0500

    Increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="miniwindeploy-heyjoeway",
-    version="1.0.0",
+    version="1.0.1",
     author="Joseph Judge",
     author_email="joe@jojudge.com",
     description="Stupid simple task runner, intended for deploying/debloating/customizing Windows ",
